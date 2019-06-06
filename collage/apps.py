a960dde198cee6d3fbf6f9e2d3fc8e3293a6d506
Nenu1985 <nenuzhny85@gmail.com>
2019-06-06 13:23:41 +0300

    deploy app on heroku through github

--- collage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CollageConfig(AppConfig):
-    name = 'collage'
--- pizzashopapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PizzashopappConfig(AppConfig):
-    name = 'pizzashopapp'
