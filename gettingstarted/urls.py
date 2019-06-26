073a942807c1a477bfaf49a0518e2524445b296c
Nenu1985 <nenuzhny85@gmail.com>
2019-06-26 12:52:43 +0300

    delete all migrations and switch off debug mode

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -55,5 +55,6 @@ urlpatterns = [
          name='password_reset_complete'),
 
 ]
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
