14d5f04c62e0168e44ad63e1308e9fe588bb3637
Nenu1985 <nenuzhny85@gmail.com>
2019-07-17 15:01:49 +0300

    Generate PDF file with orders information from admin site

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -163,7 +163,7 @@ GLOBAL_SETTINGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles\\')
 STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- orders/urls.py
+++ orders/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create/', views.order_create, name='order-create'),
     path('admin/order/<int:order_id>/', views.admin_order_detail,
          name='admin-order-detail'),
+    path('admin/order/<int:order_id>/pdf/', views.admin_order_pdf,
+         name='admin-order-pdf'),
 
 ]
