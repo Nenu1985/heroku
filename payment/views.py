d9208bcce3b6621a061aab127a0e3af69d40f73c
Nenu1985 <nenuzhny85@gmail.com>
2019-07-17 15:11:38 +0300

    Sending payment info by email with attached PDF file!

--- payment/views.py
+++ payment/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render, get_object_or_404
 from paypal.standard.forms import PayPalPaymentsForm
 from orders.models import Order
 from django.views.decorators.csrf import csrf_exempt
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+import weasyprint
+from io import BytesIO
 
 
 # Create your views here.
@@ -31,10 +36,36 @@ def payment_process(request):
                   'payment/process.html',
                   {'order': order, 'form': form})
 
+
 # We use the csrf_exempt decorator to avoid Django expecting a CSRF token, since
 # PayPal can redirect the user to any of these views via POST
 @csrf_exempt
 def payment_done(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    # create invoice e-mail
+    subject = 'My Shop - Invoice no. {}'.format(order.id)
+    message = 'Please, find attached the invoice for your recent\
+    purchase.'
+    email = EmailMessage(subject,
+                         message,
+                         'admin@myshop.com',
+                         [order.email])
+    # generate PDF
+    html = render_to_string('orders/order/pdf.html', {'order':
+                                                          order})
+    out = BytesIO()
+    stylesheets = [weasyprint.CSS(settings.STATIC_ROOT +
+                                  '\css\pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out,
+                                           stylesheets=stylesheets)
+    # attach PDF file
+    email.attach('order_{}.pdf'.format(order.id),
+                 out.getvalue(),
+                 'application/pdf')
+    # send e-mail
+    email.send()
+
     return render(request, 'payment/done.html')
 
 
