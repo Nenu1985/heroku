68d6e5ad9c0d42d9ac71153f38b6102502031443
Nenu1985 <nenuzhny85@gmail.com>
2019-07-18 09:48:46 +0300

    Coupon and cart forms have been translated.

--- coupons/forms.py
+++ coupons/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 # form that we are going to use for the user to enter a
 # coupon code
 class CouponApplyForm(forms.Form):
-    code = forms.CharField()
+    code = forms.CharField(label=_('Coupon'))
