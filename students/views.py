83288d67cc7db33792338dab1cf5656fd0dd8544
Nenu1985 <nenuzhny85@gmail.com>
2019-08-01 16:06:27 +0300

    Student's courses list and details of each course with their contents

--- students/views.py
+++ students/views.py
@@ -9,13 +9,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from students.forms import CourseEnrollForm
 from django.views.generic.list import ListView
 from courses.models import Course
+from django.views.generic.detail import DetailView
 
 
 # Create your views here.
 
 
 class StudentRegistrationView(CreateView):
-    template_name = 'students/student/registration.html'
+    template_name = 'student/registration.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('students:student-course-list')
 
@@ -49,9 +50,33 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
 
 class StudentCourseListView(LoginRequiredMixin, ListView):
+    # view for students to list the courses they are enrolled in
     model = Course
     template_name = 'students/course/list.html'
 
     def get_queryset(self):
+        # retrieving only the courses the user is enrolled in
         qs = super(StudentCourseListView, self).get_queryset()
         return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'course/detail.html'
+    def get_queryset(self):
+        qs = super(StudentCourseDetailView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentCourseDetailView,
+                        self).get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # get current module
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id'])
+        else:
+            # get first module
+            context['module'] = course.modules.all()[0]
+        return context
