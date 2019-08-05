0a5070976728fd034b41530c1e0ba48009a259cf
Nenu1985 <nenuzhny85@gmail.com>
2019-08-05 16:39:12 +0300

    Authentication

--- courses/api/views.py
+++ courses/api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Course
+from rest_framework.authentication import BasicAuthentication
 
 
 class SubjectListView(generics.ListAPIView):
@@ -19,6 +20,10 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 class CourseEnrollView(APIView):
     """Handles user enrolment in courses"""
+
+    # Users will be identified by the credentials set in the Authorization
+    # header of the HTTP request
+    authentication_classes = (BasicAuthentication,)
 
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
